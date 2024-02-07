a2e9a08de0e22794cc7c3814d6bd622d12fd752d
offish <overutilization@gmail.com>
2024-02-07 17:41:42 +0100

    stream worked for a moment

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # robot
+
+## Setup
+```bash
+python3 -m venv .venv
+soruce .venv/bin/activate
+which python
+python3 -m pip --version
+```
+
+## Installation
+```bash
+python3 -m pip install -r requirements.txt
+```
---
+++ test_camera.py
@@ -0,0 +1,28 @@
+# web.py
+
+import flask
+from v4l2py import Device
+
+app = flask.Flask("basic-web-cam")
+
+
+def gen_frames():
+    with Device.from_id(0) as cam:
+        for frame in cam:
+            yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + frame.data + b"\r\n"
+
+
+@app.route("/")
+def index():
+    return '<html><img src="/stream" /></html>'
+
+
+@app.route("/stream")
+def stream():
+    return flask.Response(
+        gen_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0")
